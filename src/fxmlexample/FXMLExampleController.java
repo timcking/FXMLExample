e461ce183419d6fdfdb52dca4f56c5c8eea2badd
timking <timcking@gmail.com>
2015-04-21 22:38:15 -0700

    Initial commit, before CSS

---
+++ src/fxmlexample/FXMLExampleController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fxmlexample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author tking
+ */
+public class FXMLExampleController {
+	@FXML private Text actiontarget;
+	
+	@FXML protected void handleSubmitButtonAction(ActionEvent event) {
+		actiontarget.setText("Sign in button pressed");
+	}
+}
